f90050f598e2de11938cea00611e908329f3c52a
Danilo Ferreira <danilof.queiroz1@gmail.com>
2023-11-28 14:54:55 -0300

    feat: create home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,28 @@
+import EventCard from '../../components/EventCard';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import ShowAllArrow from '../../components/ShowAllArrow';
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <Header />
+      <main>
+        <section>
+          <img className="h-96 w-full object-cover" src="/Banner-EJC-.jpg" alt="" />
+        </section>
+        <section className="my-8">
+          <ShowAllArrow
+            name="Eventos"
+            link="/eventos"
+            textLink="Ver todos os eventos"
+          />
+          <div className="m-8 flex basis-1/3 justify-center items-center">
+            <EventCard />
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
