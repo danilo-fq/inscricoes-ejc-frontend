6f37d21c07696f756a3a0028d75475b1d049e127
Danilo Ferreira <danilof.queiroz1@gmail.com>
2023-12-22 22:35:10 -0300

    fix: include mvp app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css';
-import Home from './pages/home';
 import SignUp from './pages/singUp';
 import SuccessInscription from './pages/successInscription';
 import NotFound from './pages/notFound';
+import Encontreiro from './pages/encontreiro';
+// import Home from './pages/home';
 // import Inscricoes from './pages/inscricoes';
 // import Login from './pages/login';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Home /> } />
-      <Route path="/inscricoes-encontreiro" element={ <SignUp /> } />
+      <Route path="/" element={ <Navigate to="/inscricoes-encontreiro" replace /> } />
+      <Route index path="/inscricoes-encontreiro" element={ <SignUp /> } />
       <Route path="/confirmacao-encontreiro" element={ <SuccessInscription /> } />
+      <Route path="/encontreiro" element={ <Encontreiro /> } />
       { /* <Route path="/login" element={ <Login /> } /> */}
       { /* <Route path="/criar-conta" element={ <SignUp /> } /> */}
       <Route path="*" element={ <NotFound /> } />
